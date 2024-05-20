52c711353564bab17b6932e86d6b9c4a8afa27b1
Chris Cashman <chris.cashman91@gmail.com>
2024-05-20 22:47:16 +0100

    Working elt pipeline with Airbyte and Airflow

--- airflow/dags/elt_dag.py
+++ airflow/dags/elt_dag.py
@@ -1,12 +1,11 @@
-from datetime import datetime, timedelta
+from datetime import datetime
 from airflow import DAG
 from docker.types import Mount  # type: ignore
+from airflow.utils.dates import days_ago  # type: ignore
+from airflow.providers.airbyte.operators.airbyte import AirbyteTriggerSyncOperator
+from airflow.providers.docker.operators.docker import DockerOperator
 
-from airflow.operators.python_operator import PythonOperator  # type: ignore
-from airflow.operators.bash import BashOperator  # type: ignore
-
-from airflow.providers.docker.operators.docker import DockerOperator  # type: ignore
-import subprocess
+CONN_ID = "125ad786-e2f3-4742-b284-435177f988b8"
 
 default_args = {
     "owner": "airflow",
@@ -14,15 +13,6 @@ default_args = {
     "email_on_failure": False,
     "email_on_retry": False,
 }
-
-
-def run_elt_script():
-    script_path = "/opt/airflow/elt_script/elt_script.py"
-    result = subprocess.run(["python", script_path], capture_output=True, text=True)
-    if result.returncode != 0:
-        raise Exception(f"Script failed with error: {result.stderr}")
-    else:
-        print(result.stdout)
 
 
 dag = DAG(
@@ -33,9 +23,13 @@ dag = DAG(
     catchup=False,
 )
 
-t1 = PythonOperator(
-    task_id="run_elt_script",
-    python_callable=run_elt_script,
+t1 = AirbyteTriggerSyncOperator(
+    task_id="airbyte_postgres_postgres",
+    airbyte_conn_id="airbyte",
+    connection_id=CONN_ID,
+    asynchronous=False,
+    timeout=3600,
+    wait_seconds=3,
     dag=dag,
 )
 
